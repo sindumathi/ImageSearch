30511a10246061cdf7910989fbbec0594e2bcd38
Sindumathi <kmsindumathi@gmail.com>
2020-09-22 16:12:54 +0530

    Update api.js

--- image-search/src/helpers/api.js
+++ image-search/src/helpers/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      'Client-ID 4c5f81d38d9d186845c8fa8d4af48d1155368271ce81a9919d2c572184fc411f',
+      'Client-ID specify-your-clientid',
   },
 });
 
